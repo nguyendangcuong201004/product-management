e9039b4f3324d7216bdb22d9a83fd1a8b2e8f801
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-03-21 17:16:53 +0700

    Setting dotenv for security

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const route = require("./routes/client/index.route.js");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 app.set("views", "./views");
 app.set('view engine', 'pug');
 
-const port = 3000;
+const port = process.env.PORT;
 
 route(app);
 
