d5673474857c5023ffa67c2b8779ac4568ca96dd
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-04-18 17:59:27 +0700

    Them moi nhom quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,6 +1,35 @@
+const Role = require("../../models/role.model.js");
+const prefixAdmin = require("../../config/system.js");
 
-module.exports.index = (req, res) => {
+// [GET] /roles
+module.exports.index = async (req, res) => {
+
+    let find = {
+        deleted: false
+    };
+
+    const records = await Role.find(find);
+
     res.render("admin/pages/roles/index.pug", {
-        pageTitle: "Trang nhóm quyền"
+        pageTitle: "Nhóm quyền",
+        records: records
     })
+}
+
+// [GET] /roles/create
+module.exports.create = (req, res) => {
+
+    res.render("admin/pages/roles/create.pug", {
+        pageTitle: "Thêm mới nhóm quyền",
+    })
+}
+
+// [POST] /roles/create
+module.exports.createPost = async (req, res) => {
+    const record = new Role(req.body);
+    await record.save();
+
+    req.flash("success", "Thêm mới quyền thành công!")
+
+    res.redirect(`${prefixAdmin}/roles`)
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const validates = require("../../validates/admin/product.validate.js")
 
 const controller = require("../../controllers/admin/role.controller.js");
 
 const router = express.Router();
 
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post("/create",
+validates.createPost
+, controller.createPost);
 
 module.exports = router;
