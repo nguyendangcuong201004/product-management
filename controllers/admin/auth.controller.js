e938f809db4b27cfc7450d540a85c238198f0c56
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-04-21 01:00:33 +0700

    Login with cookie set token public

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -1,3 +1,8 @@
+const Account = require("../../models/account.model.js");
+const md5 = require('md5');
+const prefixAdmin = require("../../config/system.js");
+
+
 // [GET] admin/auth/login
 module.exports.login = (req, res) => {
     res.render("admin/pages/auth/login.pug", {
@@ -6,7 +11,32 @@ module.exports.login = (req, res) => {
 }
 
 // [POST] admin/auth/login
-module.exports.loginPost = (req, res) => {
-    console.log(req.body);
-    res.send("OKOKM")
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email;
+    const password = md5(req.body.password);
+    const user = await Account.findOne({
+        email: email,
+        deleted: false,
+    });
+
+    if (!user){
+        req.flash("error", "Email không tồn tại !");
+        res.redirect("back");
+        return;
+    }
+
+    if (password != user.password){
+        req.flash("error", "Mật khẩu không chính xác !");
+        res.redirect("back");
+        return;
+    }
+
+    if (user.status != "active"){
+        req.flash("error", "Tài khoản đã bị khóa !");
+        res.redirect("back");
+        return;
+    }
+    
+    res.cookie("token", user.token)
+    res.redirect(`${prefixAdmin}/dashboard`)
 }
