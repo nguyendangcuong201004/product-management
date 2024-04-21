dd0c946f3e61e39a2d9abf6e35996755c9eedbbb
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-04-21 09:01:21 +0700

    Tinh nang dang xuat

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -40,3 +40,13 @@ module.exports.loginPost = async (req, res) => {
     res.cookie("token", user.token)
     res.redirect(`${prefixAdmin}/dashboard`)
 }
+
+// [GET] admin/auth/logout
+
+module.exports.logout = async (req, res) => {
+
+
+    res.clearCookie("token");
+
+    res.redirect(`${prefixAdmin}/auth/login`)
+}
--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -8,4 +8,6 @@ router.get("/login", controller.login);
 
 router.post("/login", controller.loginPost);
 
+router.get("/logout", controller.logout);
+
 module.exports = router;
