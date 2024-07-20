4a4858d1c2d46c147dcabb9f637809ade854e5bc
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-07-21 00:00:12 +0700

    Tối ưu code phần sockets

---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,53 @@
+const Chat = require("../../models/chat.model.js");
+const uploadToCloudiary = require("../../helpers/uploadToCloudiary.helper.js");
+
+
+module.exports = async (req, res) => {
+
+    const userId = res.locals.user.id;
+    const userFullName = res.locals.user.fullName;
+
+    _io.once('connection', (socket) => {
+        console.log('Có 1 user đang kết nối')
+        // CLIENT_SEND_MESSAGE
+        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+            console.log(userFullName)
+            console.log(data);
+            const images = [];
+            if (data.images.length > 0) {
+                for (const image of data.images) {
+                    const linkImage = await uploadToCloudiary(image);
+                    images.push(linkImage);
+                }
+            }
+            console.log(images);
+            const chat = new Chat({
+                user_id: userId,
+                // room_chat_id: String,
+                content: data.content,
+                images: images,
+            })
+            // Luu vào database 
+            await chat.save();
+            // Trả data realtime về client
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                user_id: userId,
+                content: data.content,
+                images: images,
+                userFullName: userFullName
+            })
+        })
+        // CLIENT_SEND_MESSAGE
+
+        // CLIENT_SEND_TYPING
+        socket.on("CLIENT_SEND_TYPING", (type) => {
+            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+                userId: userId,
+                userFullName: userFullName,
+                type: type
+            })
+        })
+        // CLIENT_SEND_TYPING
+    })
+
+}
