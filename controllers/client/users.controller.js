0f6b77e3498434973a3b03790525e7783d449f2b
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2024-07-22 09:37:20 +0700

    Render giao diện danh sách người dùng

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,9 +1,19 @@
+const User = require("../../models/user.model.js")
+
 
 // [GET] /user/not-friend
 module.exports.notFriend = async (req, res) => {
-    
+    const userId = res.locals.user.id;
+
+    const users = await User.find({
+        _id: { $ne: userId },
+        deleted: false,
+        status: "active"
+    }).select("avatar fullName")
+
 
     res.render("client/pages/users/not-friend.pug", {
-        pageTitle: "Danh sách người dùng"
+        pageTitle: "Danh sách người dùng",
+        users: users
     })
 }
